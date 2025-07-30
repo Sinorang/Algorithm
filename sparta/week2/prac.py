f30630067a11839fcb1c39ded797c1a2051e4bce
sinorang <sinholang@naver.com>
2025-07-30 16:29:59 +0900

    feat: hashtable
    - 해시테이블 알고리즘 학습

--- sparta/week2/prac.py
+++ sparta/week2/prac.py
@@ -1,16 +1,48 @@
-def test_problem_stack(s):
-    stack = []
-    pair = {")": "(", "}": "{", "]": "["}
+class HashNode:
+    def __init__(self, key=None, val=None):
+        self.key = key
+        self.val = val
+        self.next = None
 
-    for char in s:
-        if char in "({[":
-            stack.append(char)
+
+class HashTable:
+    def __init__(self):
+        self.size = 10
+        self.table = [None] * self.size
+
+    def _hash_function(self, key):
+        return key % self.size
+
+    def put(self, key, val):
+        idx = self._hash_function(key)
+        if self.table[idx] is None:
+            self.table[idx] = HashNode(key, val)
         else:
-            if not stack:
-                return False
+            node = self.table[idx]
+            while node.next is not None:
+                node = node.next
+            node.next = HashNode(key, val)
 
-            top = stack.pop()
-            if pair[char] != top:
-                return False
+    def get(self, key):
+        idx = self._hash_function(key)
+        node = self.table[idx]
+        while node is not None:
+            if node.key == key:
+                return node.val
+            node = node.next
+        return -1
 
-    return not stack
+    def remove(self, key):
+        idx = self._hash_function(key)
+        node = self.table[idx]
+        prev_node = None
+
+        while node is not None:
+            if node.key == key:
+                if prev_node is None:
+                    self.table[idx] = node.next
+                else:
+                    prev_node.next = node.next
+                return
+            prev_node = node
+            node = node.next
--- sparta/week2/test.py
+++ sparta/week2/test.py
@@ -1,5 +1,5 @@
-from func import isPalindrome, test_problem_stack
-from structures import LinkedList, Stack
+from func import isPalindrome, test_problem_queue, test_problem_stack
+from structures import HashTable, LinkedList, Queue, Stack
 
 l1 = LinkedList()
 for num in [1, 2, 1]:
@@ -39,3 +39,53 @@ assert not test_problem_stack("(]")
 assert not test_problem_stack("(()]")
 assert not test_problem_stack("(((])")
 assert not test_problem_stack("((())")
+
+
+def test_queue():
+    queue = Queue()
+
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    queue.push(4)
+    queue.push(5)
+
+    assert queue.pop() == 1
+    assert queue.pop() == 2
+    assert queue.pop() == 3
+    assert queue.pop() == 4
+    assert queue.pop() == 5
+    assert queue.pop() is None
+    assert queue.is_empty()
+
+
+assert test_problem_queue(2) == 2
+assert test_problem_queue(3) == 2
+assert test_problem_queue(4) == 4
+assert test_problem_queue(5) == 2
+assert test_problem_queue(6) == 4
+assert test_problem_queue(7) == 6
+
+
+ht = HashTable()
+
+ht.put(1, 1)
+ht.put(2, 2)
+assert ht.get(1) == 1
+assert ht.get(2) == 2
+assert ht.get(3) == -1
+
+ht.put(12, 1)
+ht.put(22, 2)
+ht.put(32, 3)
+assert ht.get(12) == 1
+assert ht.get(22) == 2
+assert ht.get(32) == 3
+
+ht.remove(12)
+assert ht.get(2) == 2
+assert ht.get(12) == -1
+assert ht.get(22) == 2
+assert ht.get(32) == 3
+
+ht.get(2)
